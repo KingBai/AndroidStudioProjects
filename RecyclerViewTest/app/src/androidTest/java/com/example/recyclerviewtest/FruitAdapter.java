77db694c6cb595f02606dc833286a1e600e17acc
lighting <396009206@qq.com>
2017-01-19 17:59:25 +0800

    ListViewTest

---
+++ RecyclerViewTest/app/src/androidTest/java/com/example/recyclerviewtest/FruitAdapter.java
@@ -0,0 +1,25 @@
+package com.example.recyclerviewtest;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by lighting on 2017/1/19.
+ */
+
+public class FruitAdapter extends RecyclerView.ViewHolder{
+
+    L
+
+
+    ImageView fruitImage;
+    TextView fruitImage;
+
+
+
+    public FruitAdapter(View itemView) {
+        super(itemView);
+    }
+}
