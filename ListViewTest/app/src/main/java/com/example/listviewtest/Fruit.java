ac3340f0b4ef42e96f99802a0241da9d1956ad70
lighting <396009206@qq.com>
2017-01-19 16:52:09 +0800

    ListViewTest

---
+++ ListViewTest/app/src/main/java/com/example/listviewtest/Fruit.java
@@ -0,0 +1,26 @@
+package com.example.listviewtest;
+
+/**
+ * Created by lighting on 2017/1/19.
+ */
+
+public class Fruit {
+
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name,int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+}
