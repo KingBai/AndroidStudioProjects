16e34c18dd4d474d9168debf4778aabea14f9548
KingBai <396009206@qq.com>
2017-02-07 23:19:00 +0800

    配置文件

---
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.fragmentbestpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by king on 2017/2/7 0007.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
