557dca8f728b5a35f3879fb8b43be389695d72c0
lighting <396009206@qq.com>
2017-02-07 18:07:48 +0800

    fragmentBestPractice

---
+++ FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/News.java
@@ -0,0 +1,28 @@
+package com.example.fragmentbestpractice;
+
+/**
+ * Created by lighting on 2017/2/7.
+ */
+
+public class News {
+
+    private String title;
+
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
